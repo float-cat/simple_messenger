9dd04a752416201897568d8246b4416b88beacad
vkaccbot <xsif8681@gmail.com>
2021-12-02 22:29:59 +0700

    мелкие правки

--- webapp/MODEL.py
+++ webapp/MODEL.py
@@ -25,7 +25,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True, nullable=False)
     email = db.Column(db.String(60), unique=True, nullable=False)
     login = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(32), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     messages = db.relationship('Message',
         primaryjoin='User.id == Message.fromUserId or User.id == Message.toUserId',
         backref='user')
@@ -44,7 +44,7 @@ class User(db.Model, UserMixin):
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self, password)
+        return check_password_hash(self.password, password)
 
 # Таблица личных сообщений пользователей
 class Message(db.Model):
