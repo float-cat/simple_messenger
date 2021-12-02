bdab447559c41ece9c76d07b2a225b956721600d
Float Cat <float.cat@yandex.ru>
2021-12-03 00:40:56 +0700

    Переименование файла FORMS.py

--- webapp/config.py
+++ webapp/config.py
@@ -3,7 +3,7 @@ import os
 'Динамический абсолютный путь к базе данных'
 print(os.path.abspath(os.path.dirname(__file__)))
 main_dir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(main_dir, '..', 'simple_messenger_database.db')}"
+SQLALCHEMY_DATABASE_URI = f"sqlite:////{os.path.join(main_dir, '..', 'simple_messenger_database.db')}"
 
 'seasurf токен (защита от кросс-скриптинга)'
 SECRET_KEY='Наш секретный ключ'
