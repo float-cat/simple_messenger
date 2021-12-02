c9e3d2fe38784f8f14a466baf02550f039c700e9
vkaccbot <xsif8681@gmail.com>
2021-12-02 12:55:07 +0700

    Конструктор

---
+++ webapp/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+from webapp.MODEL import db
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    @app.route('/')
+    def index():
+        return 'SimpleMessenger'
+    return app
