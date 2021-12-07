7bea3db7639a64c49e2a3a8351d35177792c517d
vkaccbot <xsif8681@gmail.com>
2021-12-07 11:23:04 +0700

    добавлено время жизни куки

--- webapp/config.py
+++ webapp/config.py
@@ -1,9 +1,13 @@
 import os
+from datetime import timedelta
 
 'Динамический абсолютный путь к базе данных'
 print(os.path.abspath(os.path.dirname(__file__)))
 main_dir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = f"sqlite:////{os.path.join(main_dir, '..', 'simple_messenger_database.db')}"
+SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(main_dir, '..', 'simple_messenger_database.db')}"
 
 'seasurf токен (защита от кросс-скриптинга)'
-SECRET_KEY='Наш секретный ключ'
+SECRET_KEY='123132123sfdh234dfgj'
+
+'Время жизни куки'
+REMEMBER_COOKIE_DURATION = timedelta(days=15)
