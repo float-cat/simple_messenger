1158f78d097a94cd5f6b51957f17dde9538b9389
vkaccbot <xsif8681@gmail.com>
2021-12-19 12:24:53 +0700

    исправлены мелкие недочеты

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -8,7 +8,6 @@ from webapp.modules.MessagesPage import MessagesPage
 from webapp.modules.AuthPage import AuthPage
 from webapp.modules.RegisterPage import RegisterPage
 from webapp.modules.LogoutPage import LogoutPage
-from webapp.modules.NewGroupMessagesPage import NewGroupMessagesPage
 from webapp.handlers.MessagesHandler import MessagesHandler
 from webapp.handlers.SearchUsersHandler import SearchUsersHandler
 from webapp.handlers.RegisterHandler import RegisterHandler
