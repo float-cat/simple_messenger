c9b16f07aaf5992951fce8c5d5d339094ba8ff53
vkaccbot <xsif8681@gmail.com>
2021-12-30 16:27:49 +0700

    Добавлены черные подсказки

--- webapp/static/js/tooltipsSmile.js
+++ webapp/static/js/tooltipsSmile.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Включаем подсказки
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+	
     var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
     var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
         return new bootstrap.Popover(popoverTriggerEl)
